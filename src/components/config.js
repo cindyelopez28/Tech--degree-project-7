d12ab3b297acd31328860eb30b800d74c2a4d4b6
cindyelopez28 <60763168+cindyelopez28@users.noreply.github.com>
2022-07-28 11:48:42 -0700

    Create config.js

---
+++ src/components/config.js
@@ -0,0 +1,4 @@
+const apiKey = '3c6c93a99c4131806c302fa72fc4e855';
+export default apiKey;
+
+//https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=3c6c93a99c4131806c302fa72fc4e855&tags=&text=query&per_page=24&format=json&nojsoncallback=1
